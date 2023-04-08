3ddb4cd1504f54588fde08efaab1803e999ccf32
Kevin Conner <connerk@gmail.com>
2023-04-08 11:59:48 -0700

    Initial Commit

---
+++ GitLabStatusMenuBarItem/GitLabStatusMenuBarItemApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitLabStatusMenuBarItemApp.swift
+//  GitLabStatusMenuBarItem
+//
+//  Created by Kevin Conner on 2023-04-08.
+//
+
+import SwiftUI
+
+@main
+struct GitLabStatusMenuBarItemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
